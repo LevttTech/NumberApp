ccfb50456ed2f31699030e1846244e3f976d0c98
Lev <levtokaevr@gmail.com>
2025-09-24 20:41:28 +0300

    create fragment simple logic

--- app/src/main/java/com/levtttech/numberapp/main/MainActivity.kt
+++ app/src/main/java/com/levtttech/numberapp/main/MainActivity.kt
@@ -6,16 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.levtttech.numberapp.R
+import com.levtttech.numberapp.numbers.presentation.NumbersFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.container, NumbersFragment())
+            .commit()
     }
 }
--- app/src/main/java/com/levtttech/numberapp/numbers/presentation/NumbersFragment.kt
+++ app/src/main/java/com/levtttech/numberapp/numbers/presentation/NumbersFragment.kt
@@ -1,4 +1,29 @@
 package com.levtttech.numberapp.numbers.presentation
 
-class NumbersFragment : Fragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ProgressBar
+import androidx.fragment.app.Fragment
+import androidx.transition.Visibility
+import com.levtttech.numberapp.R
+
+class NumbersFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(
+            R.layout.fragment_numbers,
+            container,
+            false
+        )
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<ProgressBar>(R.id.progressBar).visibility = View.GONE
+    }
 }
